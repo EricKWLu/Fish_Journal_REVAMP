261ce78fefc8fc11645ac663416cc6230fbea317
Eric Lu <eric.lukw@gmail.com>
2025-09-08 18:02:23 +1000

    Formatted SinglePostPage

--- client/src/routes/SinglePostPage.tsx
+++ client/src/routes/SinglePostPage.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import ImageCarousel from "../components/ImageCarousel"
+import ImageDefault from "../components/ImageDefault"
 
 const SinglePostPage = () => {
   return (
@@ -8,7 +9,8 @@ const SinglePostPage = () => {
     //Post contents
     //Sidebar with post users information
     //saved posts and delete post button on sidebar
-    <div>
+    <div className="container mx-auto px-4">
+      {/*TOP*/}
       <div className = 'flex flex-col xl:flex-row'>
         <div>
           {/*Title*/}
@@ -20,19 +22,37 @@ const SinglePostPage = () => {
             </div>
         </div>
         {/*Carousel*/}
-        <div className="xl:block xl:w-1/3">
+        <div className="xl:block xl:w-1/4">
           <ImageCarousel imagePaths={["fishPlaceholder.png", "fishPlaceholder.png", "fishPlaceholder.png"]} height={300}/>
         </div>
       </div>
 
-      <div className = 'flex flex-row mt-8'>
+      {/*BOTTOM*/}
+      <div className = 'flex flex-row mt-10'>
         {/*Content*/}
         <div className='w-full lg:w-3/4'>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eum excepturi architecto? Nobis pariatur rem labore delectus. Quos impedit, magnam quas maxime nemo numquam quis consequatur explicabo ea id iure?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sapiente non soluta nesciunt voluptas sit pariatur temporibus consectetur, recusandae iure. Enim perferendis laborum quisquam distinctio commodi pariatur dolor vero expedita.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam saepe rem error libero numquam. Temporibus obcaecati ex eaque tempora unde, cum, nam itaque minima vero aliquid, nihil iste possimus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sapiente non soluta nesciunt voluptas sit pariatur temporibus consectetur, recusandae iure. Enim perferendis laborum quisquam distinctio commodi pariatur dolor vero expedita.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam saepe rem error libero numquam. Temporibus obcaecati ex eaque tempora unde, cum, nam itaque minima vero aliquid, nihil iste possimus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sapiente non soluta nesciunt voluptas sit pariatur temporibus consectetur, recusandae iure. Enim perferendis laborum quisquam distinctio commodi pariatur dolor vero expedita.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam saepe rem error libero numquam. Temporibus obcaecati ex eaque tempora unde, cum, nam itaque minima vero aliquid, nihil iste possimus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sapiente non soluta nesciunt voluptas sit pariatur temporibus consectetur, recusandae iure. Enim perferendis laborum quisquam distinctio commodi pariatur dolor vero expedita.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam saepe rem error libero numquam. Temporibus obcaecati ex eaque tempora unde, cum, nam itaque minima vero aliquid, nihil iste possimus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sapiente non soluta nesciunt voluptas sit pariatur temporibus consectetur, recusandae iure. Enim perferendis laborum quisquam distinctio commodi pariatur dolor vero expedita.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam saepe rem error libero numquam. Temporibus obcaecati ex eaque tempora unde, cum, nam itaque minima vero aliquid, nihil iste possimus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sapiente non soluta nesciunt voluptas sit pariatur temporibus consectetur, recusandae iure. Enim perferendis laborum quisquam distinctio commodi pariatur dolor vero expedita.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam saepe rem error libero numquam. Temporibus obcaecati ex eaque tempora unde, cum, nam itaque minima vero aliquid, nihil iste possimus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sapiente non soluta nesciunt voluptas sit pariatur temporibus consectetur, recusandae iure. Enim perferendis laborum quisquam distinctio commodi pariatur dolor vero expedita.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam saepe rem error libero numquam. Temporibus obcaecati ex eaque tempora unde, cum, nam itaque minima vero aliquid, nihil iste possimus.</p>
         </div>
         {/*Sidebar*/}
-        <div className="hidden lg:block lg:w-1/4">
-          Test
+        <div className="hidden lg:flex lg:w-1/4 flex flex-col text-center ml-4 bg-red-400 sticky top-8 h-fit">
+          <p>Author</p>
+          <div className = "flex flex-row items-center justify-center gap-2 mt-4">
+            <ImageDefault src={"fishPlaceholder.png"} w={75}/>
+            <p>John Smith</p>
+          </div>
         </div>
       </div>
     </div>
