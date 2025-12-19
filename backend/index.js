9122a431697b5f01c681f5ed04e2b81c494334df
Eric Lu <eric.lukw@gmail.com>
2025-12-19 20:35:27 +1100

    Added models for mongo

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
+import connectDB from "./lib/connectDB.js"
 
 const app = express()
 
@@ -11,5 +12,6 @@ app.use("/comments", commentRouter);
 app.use("/posts", postRouter);
 
 app.listen(3000, () =>{
+    connectDB()
     console.log("Server is running")
 })
---
+++ backend/models/user.model.js
@@ -0,0 +1,37 @@
+import { Schema } from "mongoose"
+import mongoose from "mongoose"
+
+const userSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        post: {
+            type: Schema.Types.ObjectId,
+            ref: "Post",
+            required: true,
+        },
+        username: {
+            type:String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type:String,
+            required: true,
+            unique: true,
+        },
+        img: {
+            type:String,
+        },
+        savedPosts: {
+            type:Str [String],
+            default: [],
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
