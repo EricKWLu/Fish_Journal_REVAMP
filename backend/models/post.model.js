7585d09584c287a54fca4fed50a8ee4236d80410
Eric Lu <eric.lukw@gmail.com>
2026-01-01 17:01:17 +1100

    Added mutations in Write.tsx

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -15,6 +15,11 @@ const postSchema = new Schema(
             type:String,
             required: true,
         },
+        category: {
+            type:String,
+            required: true,
+            default: "general",
+        },
         slug: {
             type:String,
             required: true,
--- client/src/routes/Write.tsx
+++ client/src/routes/Write.tsx
@@ -1,10 +1,34 @@
-import {useUser} from "@clerk/clerk-react"
+import {useUser, useAuth} from "@clerk/clerk-react"
 import 'react-quill-new/dist/quill.snow.css'
 import ReactQuill from "react-quill-new"
+import { useMutation } from "@tanstack/react-query"
+import axios from "axios"
+import type React from "react"
+import { useState } from "react"
 
 const Write = () => {
 
-  const {isLoaded, isSignedIn} = useUser()
+  const {isLoaded, isSignedIn} = useUser();
+  const [value, setValue] = useState('');
+  const {getToken} = useAuth();
+
+  type NewPost = {
+    title: string;
+    category: string;
+    desc: string;
+    content: string;
+  };
+
+  const mutation = useMutation({
+    mutationFn: async (newPost: NewPost) => {
+      const token = await getToken();
+      return axios.post(`${import.meta.env.VITE_API_URL}/posts`, newPost, {
+        headers:{
+          Authorization: `Bearer ${token}`,
+        }
+      })
+    },
+  })
 
   if(!isLoaded) {
     return <div className="min-h-[calc(100vh-64px) md:min-h-[calc(100vh-80px)]">Loading...</div>
@@ -14,22 +38,47 @@ const Write = () => {
     return <div className="min-h-[calc(100vh-64px) md:min-h-[calc(100vh-80px)]">Sign In To Create A Post</div>
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const title = formData.get("title");
+    const category = formData.get("category");
+    const desc = formData.get("desc");
+
+    if (typeof title !== "string" || typeof category !== "string" || typeof desc !== "string") {
+      return;
+    }
+
+    const data = {
+      title,
+      category,
+      desc,
+      content: value,
+    };
+
+
+    console.log(data);
+
+    mutation.mutate(data)
+  }
+
   return (
     <div className='min-h-[calc(100vh-64px) md:min-h-[calc(100vh-80px)] flex flex-col gap-6'>
       <h1 className="text-xl font-light text-gray-500">Create a New Post</h1>
-      <form className="flex flex-col gap-6 mb-6">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 mb-6">
         <button className="p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white w-max">Add cover images</button>
-        <input className="text-4xl font-semibold bg-transparent outline-none text-gray-400" type="text" placeholder="My Awesome Post"/>
+        <input className="text-4xl font-semibold bg-transparent outline-none text-gray-400" type="text" placeholder="My Awesome Post" name="title"/>
         <div className="flex items-center gap-4">
           <label htmlFor="text-sm">Choose a category:</label>
-          <select name="cat" id="" className="p-2 rounded-xl bg-white shadow-md">
+          <select name="category" id="" className="p-2 rounded-xl bg-white shadow-md">
             <option value="general">General</option>
             <option value="reports">Reports</option>
             <option value="discussions">Discussions</option>
           </select>
         </div>
         <textarea className="p-4 rounded-xl bg-white shadow-md" name="desc" placeholder="A Short Description" />
-        <ReactQuill theme="snow" className="flex-1 p-2 rounded-xl bg-white shadow-md"/>
+        <ReactQuill theme="snow" className="flex-1 p-2 rounded-xl bg-white shadow-md" onChange={setValue}/>
         <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 mb-4">Send</button>
       </form>
     </div>
